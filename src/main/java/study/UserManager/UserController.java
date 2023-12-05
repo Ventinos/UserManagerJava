9911ab8f2bffc77ed36bfe9a6d4a0ffa6287afa6
Ventinos <gustavo.ventino01@gmail.com>
2023-12-04 21:31:40 -0300

    Initial Commit

---
+++ src/main/java/study/UserManager/UserController.java
@@ -0,0 +1,27 @@
+package study.UserManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @PostMapping
+    public void registerUser(@RequestBody User user){
+        userService.addNewUser(user);
+    }
+}
