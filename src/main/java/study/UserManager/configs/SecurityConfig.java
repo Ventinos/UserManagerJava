81283a742817256287941ee177702bcda9e604ef
Ventinos <gustavo.ventino01@gmail.com>
2023-12-06 17:33:34 -0300

    before leaving

--- src/main/java/study/UserManager/configs/SecurityConfig.java
+++ src/main/java/study/UserManager/configs/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                    .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
                    .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
                    .requestMatchers(HttpMethod.GET, "users").permitAll()
-                   .anyRequest().authenticated()
+                   .anyRequest().authenticated() 
        );
        return http.build();
     }
